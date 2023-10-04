b35f0b4b86162132aab272e063add6db5a89e392
coder <usmccoder@gmail.com>
2023-10-03 18:56:29 -0700

    laravel settings update

--- src/Helpers/Functions.php
+++ src/Helpers/Functions.php
@@ -2,34 +2,11 @@
 
 use SgtCoder\LaravelSettings\LaravelSettings;
 
-if (!function_exists('get_setting')) {
-    function get_setting($group, $setting = NULL)
+if (!function_exists('settings')) {
+    function settings($group = 'general')
     {
-        $settings = LaravelSettings::getSettings($group, $setting);
+        $settings = LaravelSettings::settings($group);
 
         return $settings;
-    }
-}
-
-if (!function_exists('get_settings')) {
-    function get_settings($group, $setting = NULL)
-    {
-        $settings = LaravelSettings::getSettings($group, $setting);
-
-        return $settings;
-    }
-}
-
-if (!function_exists('save_setting')) {
-    function save_setting($group, $key, $value)
-    {
-        LaravelSettings::saveSetting($group, $key, $value);
-    }
-}
-
-if (!function_exists('save_settings')) {
-    function save_settings($group, $setting)
-    {
-        LaravelSettings::saveSettings($group, $setting);
     }
 }
